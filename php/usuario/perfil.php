c317defea3d7e37d73ddfeea0b1031b4a744bea1
BenitoMarculanoRibeiro <benito2016mr@gmail.com>
2020-12-24 18:20:36 -0300

    Versão 0.0.3 - Semi funcional

--- php/usuario/perfil.php
+++ php/usuario/perfil.php
@@ -8,7 +8,7 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Guia do Programador</title>
-    <link type="text/css" rel="stylesheet" href="../css/style.css">
+    <link type="text/css" rel="stylesheet" href="../../css/style.css">
     <style>
         .init {
             display: block;
