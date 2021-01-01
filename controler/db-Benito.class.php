797387c0a3eb30915cef997f02a179629177b90f
BenitoMarculanoRibeiro <benito2016mr@gmail.com>
2021-01-01 15:28:32 -0300

    Versao 0.0.4 - Organizando arquivos

---
+++ controler/db-Benito.class.php
@@ -0,0 +1,20 @@
+<?php
+class dbb
+{
+    private $host = 'localhost';
+    private $usuario = 'root';
+    private $senha = '';
+    private $database = 'guia-do-programador';
+    public function conecta_mysql()
+    {
+        //criar conexao
+        $con = mysqli_connect($this->host, $this->usuario, $this->senha, $this->database);
+        //ajustar o charset de comunicação entre a aplicação e o banco de dados
+        mysqli_set_charset($con, 'utf8');
+        //verificar se houve erro de conexão
+        if (mysqli_connect_errno()) {
+            // echo 'Erro ao tentar se conectar com BD MySQL: ' . mysqli_connect_error();
+        }
+        return $con;
+    }
+}
---
+++ controler/vai_login.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+require_once('db.class.php');
+$email = $_POST['email'];
+$senha = md5($_POST['senha']);
+$sql = "SELECT * FROM usuario WHERE email='$email' AND senha='$senha'";
+
+$objDb = new dbb();
+$link = $objDb->conecta_mysql();
+$resultado_id  = mysqli_query($link, $sql);
+if ($resultado_id) {
+    $dados_usuario = mysqli_fetch_array($resultado_id);
+    if (isset($dados_usuario['email'])) {
+        $_SESSION['id'] = $dados_usuario['id'];
+        $_SESSION['nome'] = $dados_usuario['nome'];
+        $_SESSION['email'] = $dados_usuario['email'];
+        $_SESSION['cargo'] = $dados_usuario['cargo'];
+        //unset('nome');
+        // header('Location: ../Teste.php');
+        header('Location: ../index.php');
+    } else {
+        // header('Location: telaEtiqueta.php');
+        header('Location: ../index.php?erro=1');
+    }
+} else {
+    echo "Erro na execução da consulta, favor entrar em contato com o admin do site.<br> Whatssap: 27 995152815";
+}
